e569d28ed6cf99ed67f12b1228735f16d5fb3910
debroucker <debroucker.tommy1gmail.com>
2021-09-14 16:36:54 +0200

    back up pour dl le gdrive, le lien change h24

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from datetime import datetime
 import cal
 import pdf
 
-path = 'pdf.pdf'
+path = 'CarteRDV_DEBROUCKER_Tommy.PDF'
 gdrive_id = '1sBgUPHZIJ3zJ0l4FBDp-5v97egKMdUQg'
 
 def main() :
     #delete all event with 'Place O Permis'
     cal.delete_all_event_permis()
     #dl pdf in gdrive
-    gdd.download_file_from_google_drive(file_id=gdrive_id, dest_path='./pdf.pdf')
+    # gdd.download_file_from_google_drive(file_id=gdrive_id, dest_path='./'+path)
     #add event
     res = pdf.extract_good_text(pdf.read_pdf(path))
     for e in res : 
